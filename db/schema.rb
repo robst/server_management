9450011fada44216e0c18febd3c067d0da9f2269
robst <robertst81@gmail.com>
2016-05-14 06:34:31 +0200

    migrated

---
+++ db/schema.rb
@@ -0,0 +1,43 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20160514042629) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "company_contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "telefonnumber"
+    t.string   "mail"
+    t.integer  "company_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "company_contacts", ["company_id"], name: "index_company_contacts_on_company_id"
+
+  create_table "server_users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.integer  "server_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "server_users", ["server_id"], name: "index_server_users_on_server_id"
+
+end
