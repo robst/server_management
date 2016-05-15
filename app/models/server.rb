e524d4f6a27c10244f0e3c0c336aa98b2e31b5ed
robst <robertst81@gmail.com>
2016-05-15 12:30:45 +0200

    tests and factories

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,3 +1,8 @@
 class Server < ActiveRecord::Base
   belongs_to :company
+
+  validates :title, presence: true
+  validates :ip_address, presence: true
+  validates :company, presence: true
+
 end
--- spec/factories/servers.rb
+++ spec/factories/servers.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :server do
     title "MyString"
     ip_address "MyString"
-    company nil
+    association :company, factory: :company, strategy: :build
   end
 end
