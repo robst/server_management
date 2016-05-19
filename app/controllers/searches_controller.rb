4183d998c1eea1d0b02ebfa5c2a1ed432e2e0e3f
robst <robertst81@gmail.com>
2016-05-19 19:28:54 +0200

    removed useless actions

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,7 @@
 class SearchesController < ApplicationController
-  expose :searches, -> { Search.all }
   expose :search  
   def index; end
-
-  def new; end
-
-  def show
-  end
-
-  def create
-    search.save
-  end
+  def create; end
 
   private
 
