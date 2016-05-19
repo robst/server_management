10c8bf3fed6c4ade46a74a58beb7e493df0999c0
robst <robertst81@gmail.com>
2016-05-19 19:12:28 +0200

    search

---
+++ app/models/concerns/search.rb
@@ -0,0 +1,11 @@
+class Search
+  include ActiveModel::Model
+
+  attr_accessor :phrase
+
+  def search_results
+    %w(Server Company CompanyContact ServerUser).collect do |search_model|
+      search_model.constantize.search phrase
+    end.flatten 
+  end
+end
