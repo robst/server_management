9c4e560c4dc4af9d51dde9c0583af54f7ae2299c
robst <robertst81@gmail.com>
2016-05-23 17:46:09 +0200

    delegate company name

--- app/models/server.rb
+++ app/models/server.rb
@@ -10,6 +10,6 @@ class Server < ActiveRecord::Base
   validates :ip_address, presence: true
   validates :company, presence: true
 
-  delegate :company, to: :server, prefix: true
+  delegate :name, to: :company, prefix: true
 
 end
