3b8476fe3625b35ff5842fcd8a3d986cd63da0fd
robst <robertst81@gmail.com>
2016-05-29 05:27:17 +0200

    added association to compay_users

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ActiveRecord::Base
   include Searchable
 
   has_many :servers
+  has_many :company_contacts
   has_many :search_results, as: :searchable
 
   validates :name, presence: true
