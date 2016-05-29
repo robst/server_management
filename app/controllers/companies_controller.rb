b6c7aac497edee64fb12679ff082e0482caa4583
robst <robertst81@gmail.com>
2016-05-29 05:37:03 +0200

    added show action for company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,11 @@
 class CompaniesController < ApplicationController
   expose :companies, -> { Company.includes(servers: [:server_users]) }
+  expose :company, -> { 
+    Company.
+      includes(:company_contacts, servers: [:server_users]). 
+      find(params[:id])
+  }
+
   def index; end
+  def show; end;
 end
