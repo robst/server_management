29223bbbe73fb5b2bb28e3a52656918d8c0cdd1c
robst <robertst81@gmail.com>
2016-05-31 20:26:45 +0200

    extend specs for company

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,7 +5,9 @@ class Company < ActiveRecord::Base
   has_many :company_contacts
   has_many :search_results, as: :searchable
 
-  validates :name, presence: true
+  validates :name, 
+    presence: true,
+    uniqueness: true
 
   def company
     self
--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Company, type: :model do
   describe 'validations' do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe 'methods' do
+    it { expect(subject.company).to eq(subject) }
   end
 end
