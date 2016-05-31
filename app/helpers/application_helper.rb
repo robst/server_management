02dfeed35fb90c4bafb8b295d1453167398cedbe
robst <robertst81@gmail.com>
2016-05-31 19:58:00 +0200

    display message when 0 results

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def display_empty_message collection
+    return unless collection.empty?
+    content_tag :div, t('empty_collection'), class: 'alert alert-info'
+  end
 end
