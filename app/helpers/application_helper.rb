febe26cf96d403683a799cb1d9ce3ff68a51a3c6
robst <robertst81@gmail.com>
2016-06-02 03:32:04 +0200

    added bootstrap icon helper, changed searchform

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     return unless collection.empty?
     content_tag :div, t('empty_collection'), class: 'alert alert-info'
   end
+
+  def bootstrap_icon type
+    content_tag :span, nil, class: "glyphicon glyphicon-#{type}"
+  end
 end
