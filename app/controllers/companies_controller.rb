1f47e5cbf82121c5f28ca2ba6f0a2dbebbc9ae50
robst <robertst81@gmail.com>
2016-06-05 05:40:00 +0200

    create company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,7 +2,20 @@ class CompaniesController < ApplicationController
   expose :companies, -> { Company.includes(servers: [:server_users]) }
   expose :company
 
+  before_action :save_and_render_on_error!, only: :create
+
   def index; end
   def show; end;
   def new; end;
+  def create; end;
+
+  private
+
+  def save_and_render_on_error!
+    render :new and return unless company.save
+  end
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
 end
